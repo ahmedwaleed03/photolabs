93e3dcb4e4706c2b94b6a03982236bf7f3d250a3
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-04 16:28:41 -0400

    Added the ability to like pictures

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import PhotoFavButton from './PhotoFavButton';
 
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  /* Insert React */
   const { sampleDataForPhotoListItem } = props;
   return(
     <div className="photo-list__item">
+      <div className="photo-list__fav-button">
+        <PhotoFavButton /> 
+      </div>
       <img src={sampleDataForPhotoListItem.imageSource} className="photo-list__image" />
       <div className="photo-list__user-details">
         <img src={sampleDataForPhotoListItem.profile} className="photo-list__user-profile"/>
