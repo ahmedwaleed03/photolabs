3050106d58815c918d3bea6cc4dc24828bdf4604
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-05 17:21:33 -0400

    using mock data to create the topic list

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,29 +2,13 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "../mocks/topics.js"
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topicData, index) => (
+      {topics.map((topicData, index) => (
         <TopicListItem key={index} dataForTopicListItem={topicData} />
       ))}
     </div>
