12464aca088ceff20b873fd140b3d4c8b5501791
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-05 17:14:34 -0400

    Deleted unnecessary comments and unused data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,7 @@ import React from 'react';
 import HomeRoute from './components/HomeRoute';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = new Array(3).fill(sampleDataForPhotoListItem);
-  // const photoListItems = photos.map((data, index) => (
-  //   <PhotoListItem key={index} sampleDataForPhotoListItem={data} />
-  // ))
-
   return (
     <div className="App">
       <HomeRoute />
